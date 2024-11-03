e9438ca314c685e5f50be93484a3ae6f78f41b0c
mafaldararodrigues <ctw03650@criticaltechworks.com>
2024-11-03 23:00:31 +0000

    First commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,19 +15,24 @@
  */
 package com.example.diceroller
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,15 +40,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.IntOffset
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
+const val TAG = "DiceRollApp"
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+
+    private var gravity = FloatArray(3) { 0f }
+    private var linearAcceleration = FloatArray(3) { 0f }
+    private var lastShakeTime: Long = 0
+    private val shakeThreshold = 2.7f
+
+    private var result = mutableStateOf(1)
+
+    private lateinit var sensorManager: SensorManager
+
+    private lateinit var sensor: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        Log.d(TAG, "Sensor initialized")
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerTheme {
@@ -51,25 +73,88 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DiceRollerApp()
+                    DiceRollerApp(result.value)
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val alpha = 0.8f
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+        // Remove the gravity contribution with the high-pass filter.
+        linearAcceleration[0] = event.values[0] - gravity[0]
+        linearAcceleration[1] = event.values[1] - gravity[1]
+        linearAcceleration[2] = event.values[2] - gravity[2]
+
+        val magnitude = sqrt(
+            (linearAcceleration[0] * linearAcceleration[0] +
+                    linearAcceleration[1] * linearAcceleration[1] +
+                    linearAcceleration[2] * linearAcceleration[2])
+        )
+
+        if (magnitude > shakeThreshold) {
+            val currentTime = System.currentTimeMillis()
+
+            if (currentTime - lastShakeTime > 1000) {
+                Log.d(TAG, "Movement Detected")
+                lastShakeTime = currentTime
+                result.value = (1..6).random()
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
 }
 
-@Preview
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+fun DiceRollerApp(result: Int) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+        Box(
+            Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                }
+        ){
+            DiceWithButtonAndImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                result = result
+            )
+        }
+    }
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier,
+    result: Int
+) {
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -80,11 +165,5 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Image(painter = painterResource(imageResource), contentDescription = result.toString())
-        
-        Button(
-            onClick = { result = (1..6).random() },
-        ) {
-            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
-        }
     }
 }
